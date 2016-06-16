41b43c9001fcd1549666733714abf44568219309
wj lee <mymamyma@gmail.com>
2016-06-16 09:44:06 -0400

    first commit

---
+++ final_project/pretty_graph.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pl
+from sklearn.preprocessing import MinMaxScaler
+
+def prettyPicture(clf, X_test, y_test, feature_list):
+
+    x_min = 0.0; x_max = 1.0
+    y_min = 0.0; y_max = 1.0
+    
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+    h = .01  # step size in the mesh
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.pcolormesh(xx, yy, Z, cmap=pl.cm.seismic)
+
+    # Plot also the test points
+    grade_sig = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    bumpy_sig = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    grade_bkg = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==1]
+    bumpy_bkg = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==1]
+
+    plt.scatter(grade_sig, bumpy_sig, color = "b", label="non-poi")
+    plt.scatter(grade_bkg, bumpy_bkg, color = "r", label="poi")
+    plt.legend()
+    plt.xlabel(feature_list[1])
+    plt.ylabel(feature_list[2])
